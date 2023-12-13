b4356fa7894d64093bb0bb34b645081c9e024d0f
markrusselbaral <nullcracker04@gmail.com>
2023-12-13 23:29:41 +0800

    added announcement

--- routes/web.php
+++ routes/web.php
@@ -18,33 +18,33 @@ use App\Http\Controllers\ParentsController;
 */
 
 // PARENTS ROUTES
-// Route::middleware(['auth'])->group(function () {
-Route::get('/parent', [ParentsController::class, 'home']);
-Route::get('/parent/home', [ParentsController::class, 'home'])->name('parent.home');
-Route::get('/parent/announcements', [ParentsController::class, 'announcements'])->name('parent.announcements');
-Route::get('/parent/attendance', [ParentsController::class, 'attendance'])->name('parent.attendance');
-Route::get('/parent/grades&score', [ParentsController::class, 'grades_score'])->name('parent.grades_score');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/parent', [ParentsController::class, 'home']);
+    Route::get('/parent/home', [ParentsController::class, 'home'])->name('parent.home');
+    Route::get('/parent/announcements', [ParentsController::class, 'announcements'])->name('parent.announcements');
+    Route::get('/parent/attendance', [ParentsController::class, 'attendance'])->name('parent.attendance');
+    Route::get('/parent/grades&score', [ParentsController::class, 'grades_score'])->name('parent.grades_score');
 
 
-Route::get('/announcement', [AnnouncementController::class, 'index'])->name('announcement.index');
-Route::post('/announcement', [AnnouncementController::class, 'save'])->name('announcement.save');
-Route::get('/announcement-display', [AnnouncementController::class, 'display'])->name('announcement.display');
-Route::get('/announcement-edit/{id}', [AnnouncementController::class, 'edit'])->name('announcement.edit');
-Route::post('/announcement-update', [AnnouncementController::class, 'update'])->name('announcement.update');
-Route::delete('/announcement', [AnnouncementController::class, 'delete'])->name('announcement.delete');
+    Route::get('/announcement', [AnnouncementController::class, 'index'])->name('announcement.index');
+    Route::post('/announcement', [AnnouncementController::class, 'save'])->name('announcement.save');
+    Route::get('/announcement-display', [AnnouncementController::class, 'display'])->name('announcement.display');
+    Route::get('/announcement-edit/{id}', [AnnouncementController::class, 'edit'])->name('announcement.edit');
+    Route::post('/announcement-update', [AnnouncementController::class, 'update'])->name('announcement.update');
+    Route::delete('/announcement', [AnnouncementController::class, 'delete'])->name('announcement.delete');
 
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
-Route::get('/dashboard-display', [DashboardController::class, 'display'])->name('dashboard.display');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+    Route::get('/dashboard-display', [DashboardController::class, 'display'])->name('dashboard.display');
 
 
-Route::get('/user', [UserController::class, 'index'])->name('user.index');
-Route::post('/user', [UserController::class, 'save'])->name('user.save');
-Route::get('/user-display', [UserController::class, 'display'])->name('user.display');
-Route::get('/user-edit/{id}', [UserController::class, 'edit'])->name('user.edit');
-Route::put('/user', [UserController::class, 'update'])->name('user.update');
-Route::delete('/user', [UserController::class, 'delete'])->name('user.delete');
-// });
+    Route::get('/user', [UserController::class, 'index'])->name('user.index');
+    Route::post('/user', [UserController::class, 'save'])->name('user.save');
+    Route::get('/user-display', [UserController::class, 'display'])->name('user.display');
+    Route::get('/user-edit/{id}', [UserController::class, 'edit'])->name('user.edit');
+    Route::put('/user', [UserController::class, 'update'])->name('user.update');
+    Route::delete('/user', [UserController::class, 'delete'])->name('user.delete');
+});
 
 
 Auth::routes();
